d45ba0aa6c7395249eb3a3026cf161ede19252f6
ranawp <ranasaha4224#gmail.com>
2022-07-10 15:10:09 +0600

    milstones added

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
 const milestoneData = JSON.parse(data).data
-console.log(milestoneData)
+
+
+//load course milstone data 
+function loadMilstones() {
+    const milestones = document.querySelector('.milestones');
+    milestones.innerHTML = `${milestoneData.map(function (milestone) {
+        return `<div class="milestone border-b"> 
+        <div class="flex"> 
+            <div class="checkbox">
+                <input type="checkbox">   
+            </div> 
+        <div>
+        <p>
+           ${milestone.name}
+            <span><i class="fas fa-chevron-down"> </i> </span>
+        </p> 
+        </div>
+        </div> 
+        <div class='hidden_panel'> 
+            <div class="module border-b"> 
+           ${milestone.modules.map(function (module) {
+            return `<div class="module border-b">
+            <p>${module.name}</p>
+            </div>`
+        }).join("")}
+        </div>
+        </div>
+        </div>`
+    }).join("")}`
+}
+
+loadMilstones()
